e10e91b591c429355a7f568d09811ec80165ea58
pranjaldubal <pranjal.dind15@sot.pdpu.ac.in>
2018-08-17 13:07:55 +0530

    till lunch break

---
+++ logisticregression.R
@@ -0,0 +1,29 @@
+#Logistic regression
+
+library(ISLR)
+?Default
+data(Default)
+head(Default, n=30)
+str(Default)
+head(Default[Default$default=='Yes',])
+table(Default$student)
+table(Default$default)
+t1=table(Default$default, Default$student)
+addmargins(t1)
+addmargins(t1, FUN = mean)
+library(dplyr)
+Default%>%group_by(student, default) %>% summarise(n())
+Default%>%group_by(student, default) %>% summarise(mean(income), mean(balance))
+
+#logistic model
+logit1= glm(default~income+balance+student, family = 'binomial', data = Default)
+summary(logit1) #income is not important in result so we remove the income
+logit2= glm(default~balance+student, family = 'binomial', data = Default)
+summary(logit2)
+ndata1= data.frame(balance=mean(Default$balance), student='Yes')
+predict(logit2, newdata = ndata1, type = 'response') 
+(ndata1= data.frame(balance=mean(Default$balance), student='Yes'))
+(p1=predict(logit2, newdata = ndata1, type = 'response'))
+(ndata1= data.frame(balance=1486, student='Yes'))
+(p1=predict(logit2, newdata = ndata1, type = 'response'))
+  
